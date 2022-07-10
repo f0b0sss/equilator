01036b7e9acc598f799e7c0289752ca8eec37c95
Dmitriy Zimin <dmitriy3imin@gmail.com>
2022-07-10 12:01:52 +0300

    add error page

---
+++ domain/src/main/java/com/equilator/services/Calc2.java
@@ -0,0 +1,57 @@
+package com.equilator.services;
+
+public class Calc2 {
+    /*
+
+    private volatile int c;
+
+    public synchronized void value(){
+        System.out.println("Counter = " + c);
+    }
+
+    public Calc2() {
+        c = 0;
+    }
+
+    public synchronized void add(){
+        c++;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Calc2 calc2 = new Calc2();
+
+        Thread incThread = new Thread(() -> {
+            System.out.println("start 1");
+            for (int i = 0; i < 1000000; i++) {
+                calc2.add();
+            }
+            System.out.println("end 1");
+        });
+
+        Thread decThread = new Thread(() -> {
+            while (incThread.isAlive()){
+                System.out.println("start 2");
+                calc2.value();
+                System.out.println("end 2");
+            }
+        });
+
+        try {
+            incThread.start();
+            decThread.start();
+            incThread.join();
+            decThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+     */
+
+
+}
